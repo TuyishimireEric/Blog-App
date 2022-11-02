3f863d5c6729deeb41c8065981ec5c61b082c4a9
Eric <tuyishimireericc@gmail.com>
2022-11-02 15:45:29 +0200

    added controllers spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+    before(:example) do
+      get '/'
+    end
+
+    it 'return correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should respond with a correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'body should have correct  placeholder text' do
+      expect(response.body).to include('Here is a list of users')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) do
+      get '/users/1'
+    end
+
+    it 'return correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('Here is an about of given user')
     end
   end
 end
